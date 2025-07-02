b0efb5cba55890749745aece29933195c0de0cbb
SATO Soma <s1320122@sshsv211.u-aizu.ac.jp>
2025-07-03 05:21:14 +0900

    Won/lost message added

--- Dicegame.java
+++ Dicegame.java
@@ -2,10 +2,12 @@ import java.util.Random;
 import java.util.Scanner;
 public class Dicegame{
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your name? > ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
+System.out.println("What is your name?");
+System.out.print("> ");
+Scanner scanner = new Scanner(System.in);
+String name = scanner.nextLine();
+System.out.println("Hello, " + name + "!");
+
     int Total = 0;
     Random random = new Random();
     System.out.println("Rolling dice...");
@@ -15,5 +17,11 @@ public static void main(String[] args) {
     System.out.println("Die " + (i+1) + ": " + diceRoll); 
     }
     System.out.println("Total value: " + Total);
+    if (Total > 7) {
+    System.out.println(name+ " won!");
+}
+ else{
+    System.out.println(name + " lost!");
+}
 }
 }
