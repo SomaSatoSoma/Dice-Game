ab91b95477884c2c49e158c385b2c0ff26d794dc
SATO Soma <s1320122@sshsv211.u-aizu.ac.jp>
2025-07-03 05:18:42 +0900

    Greetings added

--- Dicegame.java
+++ Dicegame.java
@@ -1,6 +1,11 @@
 import java.util.Random;
+import java.util.Scanner;
 public class Dicegame{
-
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? > ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
     int Total = 0;
     Random random = new Random();
     System.out.println("Rolling dice...");
