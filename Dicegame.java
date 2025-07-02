8ff6e5ef859b6196d4a5db0bfd6542fe7414a0e3
SATO Soma <s1320122@sshsv211.u-aizu.ac.jp>
2025-07-03 05:17:06 +0900

    Dice game: initial revision

--- Dicegame.java
+++ Dicegame.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 public class Dicegame{
-public static void main(String[] args) {
+
     int Total = 0;
     Random random = new Random();
     System.out.println("Rolling dice...");
     for(int i = 0;i < 2;i++){
     int diceRoll = random.nextInt(6) + 1;
     Total += diceRoll;
-    System.out.println("Die " + i+1 + ": " + diceRoll); 
+    System.out.println("Die " + (i+1) + ": " + diceRoll); 
     }
     System.out.println("Total value: " + Total);
 }
